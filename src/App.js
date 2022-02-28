619408f60a0f3065021ca9ffb1b07b141c8db9c8
ganaranjish <gana.g1989@gmail.com>
2022-02-28 17:52:45 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import Home from './pages/Home';
+import {Button} from 'antd';
+import JobInfo from './pages/JobInfo';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppliedJobs from './pages/AppliedJobs';
+import PostJob from './pages/PostJob';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+    <Route path='/'exact element={<Home/>}/>
+    <Route path='/appliedjobs'exact element={<AppliedJobs/>}/>
+    <Route path='/postjob'exact element={<PostJob/>}/>
+    <Route path='/profile'exact element={<Profile/>}/>
+
+    </Routes>
     </div>
+    </Router>
   );
 }
 
